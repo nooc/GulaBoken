ec94bbe493505baee3dbd29e6e881bbabfa90abf
Ben Bright <nooc@users.noreply.github.com>
2022-11-01 10:23:03 +0100

    user database work

--- GulaBoken/src/main/java/yh/gulaboken/IUserDatabase.java
+++ GulaBoken/src/main/java/yh/gulaboken/IUserDatabase.java
@@ -1,5 +1,18 @@
 package yh.gulaboken;
 
 public interface IUserDatabase {
+    /**
+     *
+     * @param username
+     * @return User
+     */
+    IUser getUser(String username);
+
+    /**
+     *
+     * @param username
+     * @param password
+     * @return Authenticated user
+     */
     IUser authenticate(String username, String password);
 }
--- GulaBoken/src/main/java/yh/gulaboken/userdatabase/HardcodedUserDatabase.java
+++ GulaBoken/src/main/java/yh/gulaboken/userdatabase/HardcodedUserDatabase.java
@@ -5,16 +5,39 @@ import yh.gulaboken.IUserDatabase;
 
 class HardcodedUserDatabase implements IUserDatabase {
 
+    /**
+     * List of hard coded users.
+     */
     private final IUser[] users;
 
+    /**
+     * Constructor
+     * Create list of hard coded users.
+     */
     HardcodedUserDatabase() {
         this.users = new IUser[] {
-                new User("admin", "password")
+                new User("guest", "guest", false),
+                new User("admin", "secret", true)
         };
     }
 
+    /**
+     * Get user with username.
+     * @param username
+     * @return IUser
+     */
+    @Override
+    public IUser getUser(String username) {
+    }
+
+    /**
+     * Authenticate user with username and password and return the authenticated user.
+     * If user doesn't exist or authentication fails, return null.
+     * @param username
+     * @param password
+     * @return IUser
+     */
     @Override
     public IUser authenticate(String username, String password) {
-        return null;
     }
 }
